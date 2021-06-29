6ccb9399af3a1b84410fa93a93b0ff9a2023c025
doyeon <doyeon311@gmail.com>
2021-06-30 00:06:31 +0900

    fix code

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,9 +3,9 @@ import React from "react";
 class Detail extends React.Component {
   componentDidMount() {
     const { location, history } = this.props;
-    // if (location.state === undefined) {
-    history.push("/");
-    // }
+    if (location.state === undefined) {
+      history.push("/");
+    }
   }
   render() {
     const { location } = this.props;
