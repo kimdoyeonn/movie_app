5b4925b690ec9089a2063e8eccc4e934d344178f
doyeon <doyeon311@gmail.com>
2021-07-25 23:05:19 +0900

    refec: Loader Component

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styled from "styled-components";
+
+const Container = Styled.div``;
+
+const Loader = () => (
+  <Container>
+    <span>Loading...</span>
+  </Container>
+);
+
+export default Loader;
