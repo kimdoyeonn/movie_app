d1991d9ff454f31ff78f667966b3f63a19bd6443
doyeon <doyeon311@gmail.com>
2021-07-25 23:14:01 +0900

    refec: import 정리

--- src/routes/Home/HomeContainer.js
+++ src/routes/Home/HomeContainer.js
@@ -1,7 +1,5 @@
 import React from "react";
 import axios from "axios";
-// import Movie from "../Components/Movie.js";
-import "./Home.css";
 import HomePresenter from "./HomePresenter";
 
 class Home extends React.Component {
